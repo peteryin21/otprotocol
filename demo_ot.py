68dc5a15c765a56b0d5a016ad4558b96eade8ac8
Peter Yin <py.peteryin@gmail.com>
2015-06-12 22:28:07 -0500

    Change to ordered dicts, ingredients function, fixed underscores

--- demo_ot.py
+++ demo_ot.py
@@ -4,40 +4,43 @@ from otprotocol.protocol import Protocol, Head
 #instantiate new Protocol object
 p = Protocol()
 
-#append decks to Protocol object
-p200_rack = p.deck("p200_rack", labware="tiprack-200ul")
-trough = p.deck("trough", labware="trough-12row")
-plate_1 = p.deck("plate_1", labware="96-flat")
-plate_2 = p.deck("plate_2", labware="96-flat")
-trash = p.deck("trash", labware="point")
-
 #create head object
 p200 = Head( "p200", #name
 	{#opts
 	"tool" : "pipette", 
-	"tip_racks" : [{"container":"p200_rack"}],
-	"trash_container" : {"container":"trash"},
-	"multi_channel" : True,
+	"tip-racks" : [{"container":"p200-rack"}],
+	"trash-container" : {"container":"trash"},
+	"multi-channel" : True,
 	"axis" : "a",
 	"volume" : 250,
-	"down_plunger_speed" : 300,
-	"up_plunger_speed" : 500,
-	"tip_plunge" : 8,
-	"extra_pull_volume" : 20,
-	"extra_pull_delay" : 200,
-	"distribute_percentage" : 0.1,
+	"down-plunger-speed" : 300,
+	"up-plunger-speed" : 500,
+	"tip-plunge" : 8,
+	"extra-pull-volume" : 20,
+	"extra-pull-delay" : 200,
+	"distribute-percentage" : 0.1,
 	"points" : [{"f1": 10,"f2": 6},{"f1": 25,"f2": 23},{"f1": 50,"f2": 49},{"f1": 200,"f2": 200}]
 		}
 	)
+
+#append decks to Protocol object
+p200_rack = p.deck("p200-rack", labware="tiprack-200ul")
+trough = p.deck("trough", labware="trough-12row")
+plate_1 = p.deck("plate-1", labware="96-flat")
+plate_2 = p.deck("plate-2", labware="96-flat")
+trash = p.deck("trash", labware="point")
+
+#add ingredients
+p.add_ingredient("ReagentA", "Reagents-1", "A3", 25000)
 
 #add head object to protocol
 p.set_head(p200)
 
 #append instructions 
-p.transfer("trough","A1","plate_1","A1",100)
-p.transfer("trough", ["A1","A2"],"plate_1", ["B1","B2"],[100,20])
-p.distribute("trough","A1","plate_1",["A1","A2","A3"],[20, 30, 100])
-p.consolidate("plate_1",["A1","A2","A3"],"plate_2","A1",[20, 30, 100])
-p.mix("plate_2", "A1", 20)
+p.transfer("trough","A1","plate-1","A1",100)
+p.transfer("trough", ["A1","A2"],"plate-1", ["B1","B2"],[100,20])
+p.distribute("trough","A1","plate-1",["A1","A2","A3"],[20, 30, 100])
+p.consolidate("plate-1",["A1","A2","A3"],"plate-2","A1",[20, 30, 100])
+p.mix("plate-2", "A1", 20)
 
 print json.dumps(p.as_dict(), indent=2)
--- otprotocol/protocol.py
+++ otprotocol/protocol.py
@@ -1,4 +1,5 @@
 import json
+from collections import OrderedDict
 
 class Deck(object):
     """Elements of OT deck"""
@@ -12,18 +13,25 @@ class Head(object):
 		self.name = name
 		self.opts = opts
 
+class Ingredient(object):
+	"""Ingredients in OT for liquid-tracking"""
+	def __init__(self, name, opts):
+		self.name = name
+		self.opts = opts
+
 class Protocol(object):
 	""" Sequence of instructions to be executed and set of deck elements 
 		on which instructions act"""
-	def __init__(self, decks=[], head=None, instructions=None):
-		self.decks = {}
+	def __init__(self, decks=[], head=None, instructions=None, ingredients=None):
+		self.decks = OrderedDict()
 		for deck in decks:
 			self.decks[deck.name] = deck
 		self.instructions = instructions or []
+		self.ingredients = OrderedDict()
 
 	def deck(self, name, labware):
 		"""Append a Deck object to deck list associated with protocol."""
-		opts = {}
+		opts = OrderedDict()
 		opts["labware"] = labware
 		#opts["slot"] = slot
 		self.decks[name] = Deck(name, opts)
@@ -31,19 +39,42 @@ class Protocol(object):
 	def set_head(self, head):
 		self.head = head
 
+	def add_ingredient(self, name, container, location, volume):
+		"""Append ingredient object to ingredients associated with protocol"""
+		opts = OrderedDict()
+		opts["container"] = container
+		opts["location"] = location
+		opts["volume"] = volume
+
+		self.ingredients[name] = Ingredient(name, opts)
 	def as_dict(self):
 		"""Return protocol as a dictionary"""
-		return {
-			"deck" : dict(
+		d = OrderedDict()
+		d["head"] = {self.head.name: self.head.opts}
+		d["deck"] = dict(
 				(key,value.opts)
-				for key, value in self.decks.items()),
-			"head" : {self.head.name: self.head.opts},
-
-			"instructions": [{
-							"tool":self.head.name,
-							"groups": self.instructions
+				for key, value in self.decks.items())
+		d["ingredients"] = dict(
+				(key,[value.opts])
+				for key, value in self.ingredients.items())
+		d["instructions"] = [{
+						"tool":self.head.name,
+						"groups": self.instructions
 							}]
-		}
+		return d
+		# return {
+		# 	"deck" : dict(
+		# 		(key,value.opts)
+		# 		for key, value in self.decks.items()),
+		# 	"head" : {self.head.name: self.head.opts},
+		# 	"ingredients" : dict(
+		# 			(key,[value.opts])
+		# 			for key, value in self.ingredients.items()),
+		# 	"instructions": [{
+		# 					"tool":self.head.name,
+		# 					"groups": self.instructions
+		# 					}]
+		# }
 
 	def transfer(self, source, source_well, dest, dest_well, volume, touch_tip=True, blowout=True, extra_pull=True, tip_offset=-2, delay=2000):
 		if not isinstance(source_well,list):
@@ -55,24 +86,41 @@ class Protocol(object):
 		trans = {}
 		trans["transfer"] = []
 		for a, b, v in zip(source_well, dest_well, volume):
-			x = {
-				"from": {
+			x = OrderedDict()
+			x["from"] = {
 					"container": source,
 					"location": a,
 					"tip-offset": tip_offset,
 					"delay": delay,
 					"touch-tip": touch_tip
-				},
-
-				"to": {
+				}
+			x["to"] = {
 					"container": dest,
 					"location": b,
 					"touch-tip": touch_tip
-				},
-				"volume": v,
-				"blowout": blowout,
-				"extra-pull": extra_pull
-			}
+				}
+			x["volume"] = v
+			x["blowout"] = blowout
+			x["extra-pull"] = extra_pull
+
+			# x = {
+			# 	"from": {
+			# 		"container": source,
+			# 		"location": a,
+			# 		"tip-offset": tip_offset,
+			# 		"delay": delay,
+			# 		"touch-tip": touch_tip
+			# 	},
+
+			# 	"to": {
+			# 		"container": dest,
+			# 		"location": b,
+			# 		"touch-tip": touch_tip
+			# 	},
+			# 	"volume": v,
+			# 	"blowout": blowout,
+			# 	"extra-pull": extra_pull
+			# }
             
 			trans["transfer"].append(x)
 
@@ -86,24 +134,37 @@ class Protocol(object):
 			volumes = [volumes]*len(dest_wells)
 
 		for well, vol in zip(dest_wells, volumes):
-			d = {
-				"container": dest,
-				"location": well,
-				"volume": vol,
-				"touch-tip":touch_tip
-			}
-			dest_groups.append(d)
+			d = OrderedDict()
+			d["container"] = dest
+			d["location"] = well
+			d["volume"] = vol
+			d["touch-tip"] = touch_tip
 
-		x = { 	
-			"from": {
+			# d = {
+			# 	"container": dest,
+			# 	"location": well,
+			# 	"volume": vol,
+			# 	"touch-tip":touch_tip
+			# }
+			dest_groups.append(d)
+		x = OrderedDict()
+		x["from"] = {
 				"container": source,
 				"location": source_well,
+			}
+		x["to"] = dest_groups
+		x["blowout"] = blowout
 
-			},
+		# x = { 	
+		# 	"from": {
+		# 		"container": source,
+		# 		"location": source_well,
 
-			"to": dest_groups,
-			"blowout": blowout,
-		}
+		# 	},
+
+		# 	"to": dest_groups,
+		# 	"blowout": blowout,
+		# }
 
 		dist = {}
 		dist["distribute"] = x
@@ -117,38 +178,60 @@ class Protocol(object):
 			volumes = [volumes]*len(source_wells)
 
 		for well, vol in zip(source_wells, volumes):
-			d = {
-				"container": source,
-				"location": well,
-				"volume": vol,
-				"touch-tip":touch_tip
-			}
+			d = OrderedDict()
+			d["container"] = source
+			d["location"] = well
+			d["volume"] = vol
+			d["touch-tip"] = touch_tip
+
+			# d = {
+			# 	"container": source,
+			# 	"location": well,
+			# 	"volume": vol,
+			# 	"touch-tip":touch_tip
+			# }
 			source_groups.append(d)
 
-		x = { 	
-			"from": source_groups,
-
-			"to": {
+		x = OrderedDict()
+		x["from"] = source_groups,
+		x["to"] = {
 				"container": dest,
 				"location": dest_well,
+			}
+		x["blowout"] = blowout
 
-			},
-			"blowout": blowout,
-		}
+		# x = { 	
+		# 	"from": source_groups,
+
+		# 	"to": {
+		# 		"container": dest,
+		# 		"location": dest_well,
+
+		# 	},
+		# 	"blowout": blowout,
+		# }
 
 		cons = {}
 		cons["consolidate"] = x
 		self.instructions.append(cons)
 
-	def mix(self, source, source_well, volume, repetitions=5, blowout=True, liquid_tracking=True):     
-		x = {
-			"conatiner": source,
-			"location": source_well,
-			"volume": volume,
-			"repetitions": repetitions,
-			"blowout": blowout,
-			"liquid-tracking": liquid_tracking
-		}
+	def mix(self, source, source_well, volume, repetitions=5, blowout=True, liquid_tracking=True):  
+		x = OrderedDict()
+		x["container"] = source
+		x["location"] = source_well
+		x["volume"] = volume
+		x["repetitions"] = repetitions
+		x["blowout"] = blowout
+		x["liquid-tracking"] = liquid_tracking
+
+		# x = {
+		# 	"container": source,
+		# 	"location": source_well,
+		# 	"volume": volume,
+		# 	"repetitions": repetitions,
+		# 	"blowout": blowout,
+		# 	"liquid-tracking": liquid_tracking
+		# }
 
 		mix = {}
 		mix["mix"] = [x]
